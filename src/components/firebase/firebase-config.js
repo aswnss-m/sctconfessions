16ae34bd01073fc9fe1b0e819af36440c6ee5731
aswnss <aswnss.m@gmail.com>
2023-02-02 10:23:35 +0530

    added firestore connections

---
+++ src/components/firebase/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJtlg1rtLvwYl9fXrJ_PkWOrZjyQKLkd8",
+  authDomain: "sctconfession-1807f.firebaseapp.com",
+  projectId: "sctconfession-1807f",
+  storageBucket: "sctconfession-1807f.appspot.com",
+  messagingSenderId: "610304282711",
+  appId: "1:610304282711:web:e96b542b48996017009312",
+  measurementId: "G-PJENH8K80X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+// const analytics = getAnalytics(app);
